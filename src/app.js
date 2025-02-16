24b683f07e5e15e21dcbf2d3f3432680c955aca8
SKrishna891 <krishna70720@gmail.com>
2025-02-15 21:10:37 -0500

    updatebyId email and Delete data

--- src/app.js
+++ src/app.js
@@ -8,6 +8,41 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/user", async (req,res) => {
+   const userEmail = req.body.email;
+   try{
+      const user = await User.findOne({email : userEmail}); 
+   if(!user){
+      res.status(500).send("user data not saved");
+   }
+   else{
+      res.send(user); 
+   }
+   }
+   catch(err){
+      res.status(401).send("usernotfound");
+   }
+
+});
+
+app.get("/feed", async (req,res) => {
+   try{
+   const user = await User.find({});
+   res.send(user);
+   }catch(err){
+      res.status(401).send("usernotfound");
+   }
+});
+
+app.get("/byid", async (req,res) => {
+const userId = req.body.userId;
+   try {
+const user = await User.findById(userId);
+res.send(user);
+   }catch(err){
+      res.status(401).send("usernotfound");
+   }
+});
 app.post("/signup", async (req,res) => {
 
  const user = new User(req.body);
@@ -20,6 +55,40 @@ app.post("/signup", async (req,res) => {
    res.status(500).send("user data not saved");
  }
 
+});
+
+app.delete("/user",async (req, res) => {
+
+   const  userId = req.body.userId;
+   try{
+      const user = await User.findByIdAndDelete(userId);
+      res.send("user deleted sucessfully");
+
+   }catch(err){
+   res.status(500).send("user data not saved");
+ }
+
+});
+
+app.patch("/user", async (req,res) => {
+const userId = req.body.userId;
+const data = req.body;
+try{
+const user = await User.findByIdAndUpdate(userId,data);
+res.send("userupdated sucessfully");
+}catch(err){
+   res.status(500).send("user data not saved");
+ }
+});
+app.patch("/useremail", async(req,res) => {
+const emailId = req.body.email;
+const data = req.body;
+try{
+const user = await User.findOneAndUpdate({ email: emailId },data);
+res.send("data updated sucessfully");
+}catch(err){
+   res.status(500).send("user data not saved");
+ }
 });
 
 connectDB()
--- src/models/user.js
+++ src/models/user.js
@@ -19,4 +19,4 @@ type : Number
     
 });
 
-module.exports = mongoose.model("User",UserSchema )
+module.exports = mongoose.model("User",UserSchema );
